15133a2d8edef26baa06081ca620f40c2f7392c2
nqnduy <nqn.duy@gmail.com>
2024-04-20 18:31:53 +0700

    Update opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "vi",
-        locales: ["vi", "en"],
-    }
-})
+  i18n: {
+    defaultLocale: "vi",
+    locales: ["vi", "en"]
+  },
+  output: "server",
+  adapter: vercel()
+});
